695db5dd3ea5c3fad05e5bfcb5603d999dd41ddf
Sandip Chaudhary <87557535+thesandipchaudhary@users.noreply.github.com>
2022-12-09 13:56:57 +0545

    Add files via upload

---
+++ comparison.py
@@ -0,0 +1,24 @@
+#comparision operater
+#comparision opertor returns boolean value which is either true or false
+a=(5==4)
+print(a)
+
+
+b=(3!=4)
+print(b)
+
+
+c=(5>=4)
+print(c)
+
+
+d=(5<=4)
+print(d)
+
+a=(5>4)
+print(a)
+
+a=(5<4)
+print(a)
+
+
